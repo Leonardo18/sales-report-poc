7babbaa6d41f613319a0ac3d617c3ad456a093e9
Leonardo Dutra <leonardo.anjos@agibank.com.br>
2019-04-18 17:35:58 -0300

    adding customer builder class

---
+++ src/main/java/com/poc/sales.report/builder/CustomerBuilder.java
@@ -0,0 +1,34 @@
+package com.poc.sales.report.builder;
+
+import com.poc.sales.report.model.CustomerModel;
+
+public class CustomerBuilder {
+
+    private CustomerModel customerModel = new CustomerModel();
+
+    CustomerBuilder() { }
+
+    public CustomerBuilder withId(Integer id){
+        this.customerModel.setId(id);
+        return this;
+    }
+
+    public CustomerBuilder withTaxId(String taxId){
+        this.customerModel.setTaxId(taxId);
+        return this;
+    }
+
+    public CustomerBuilder withName(String name){
+        this.customerModel.setName(name);
+        return this;
+    }
+
+    public CustomerBuilder withBusinessArea(String businessArea){
+        this.customerModel.setBusinessArea(businessArea);
+        return this;
+    }
+
+    public CustomerModel build(){
+        return this.customerModel;
+    }
+}
